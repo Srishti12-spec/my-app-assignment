91c67296475754d88ae15edc344187382169245f
Srishti Pal <palsris692@gmail.com>
2023-10-29 16:09:24 +0530

    na

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logo from './assets/login-form.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,60 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="section">
+
+        <div className='row'>
+
+          <div className='col-1-2'>
+            <img src={logo} id='login-logo' alt="logo" />
+          </div>
+
+          <div className='col-1-2'>
+            <div id='login-form'>
+
+              <p className='input-text'>Login ID</p>
+              <div className='input-txt'>
+
+                <input type='text' className='login-input' placeholder='Login ID' />
+              </div>
+              <p className='input-text'>Password</p>
+              <div className='input-txt'>
+
+                <input type='text' className='login-input' placeholder='Password' />
+              </div>
+              <div className='row login-checks'>
+                <div className='col'>
+                  <div className='row rem-me'>
+
+                    <input type='checkbox' />
+                    <p className='login-check-text'>Remember me</p>
+                  </div>
+                </div>
+                <div className='col'>
+                  <a href='#' id='change-psw' >
+                    <p className='link-text'>Change Password</p>
+                  </a>
+                </div>
+              </div>
+              <div className='row agree-tc'>
+
+                <input type='checkbox' />
+                <p className='login-check-text'>Agree to <a href='#' id='agree' >Terms and Conditions</a></p>
+              </div>
+
+              <div className='btn-wrap'>
+
+                <input type='button' id='login-btn' value={'Login'} />
+              </div>
+              <p className='login-check-text'>Don't have an account? <a href='#' id='reg' >Register here</a></p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
